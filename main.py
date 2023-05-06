d05dec11ac3017c736693d75029319d147bb7517
jacksparrow124 <smithparkchicagodfa@gmail.com>
2023-05-06 14:09:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
 import whisperload
+import whisper 
+
+on = True
+if on == True:
+
+    print("Loading / Updating whisper models...")
+    cont = False
+
+
+    available_models = whisper.available_models()
+
+
+
+    print("Available models: " + str(available_models))
+    def load_model():
+        load_model = import whisper.available_models
+    model = whisper
+    for model in available_models:
+        print("Downloading model: " + model)
+        whisper.load_model()
+
+    print("Done!")
+
+    if print ('Done'):
+     cont = True 
+
+
+
+
+
+
+
+
+
+
 
 
 
