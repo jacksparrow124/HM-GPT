959b19bbb9b92a1808c3b4b54a1e69027116eb82
jacksparrow124 <smithparkchicagodfa@gmail.com>
2023-05-06 14:02:08 -0700

    Update whisperload.py

--- whisperload.py
+++ whisperload.py
@@ -1,20 +1,21 @@
-import whisperload
+import whisper
 
 print("Loading / Updating whisper models...")
+cont = False
 
-
-available_models = whisperload.available_models()
+available_models = whisper.available_models()
 
 print("Available models: " + str(available_models))
 def load_model():
     import whisperload 
-model = whisperload 
+model = whisper
 for model in available_models:
     print("Downloading model: " + model)
-    whisperload.load_model()
+    whisper.load_model()
 
 print("Done!")
-
+if print ('Done'):
+    cont = True 
 
 
 
