580d676624ba7d86fa08b8296ab859b8646b4dc3
jacksparrow124 <smithparkchicagodfa@gmail.com>
2023-05-08 18:51:50 -0700

    Update main.py

--- main/main.py
+++ main/main.py
@@ -2,24 +2,28 @@ import os
 from dotenv import load_dotenv
 import openai
 
+
+
+
+
 load_dotenv()
 
-openai.api_key = os.getenv("Your openai key 🙂 requires paid account")
+openai.api_key = os.getenv("your openai account key")
+
+
+print(openai.api_key)
 
 
 
 
 
+    #def speechToText(mp3):
+    #   input = audio_file("path/to/file/audio.mp3")
+    #  audio_file = open(input)
+    # transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
-
-
-#def speechToText(mp3):
- #   input = audio_file("path/to/file/audio.mp3")
-  #  audio_file = open(input)
-   # transcript = openai.Audio.transcribe("whisper-1", audio_file)
-
-#def textToSpeech(text):
-    
+    #def textToSpeech(text):
+        
 
 
 def chat(input): 
@@ -28,21 +32,26 @@ def chat(input):
 
     messages=[{"role": "user", "content": input}]
 
-    messages=[{"role": "user", "content": "%i"}%input]
+    #messages=[{"role": "user", "content": "%i"}%input]
 
     )
-    print(completion)
 
-chat("what is the capital of france?")
+    return completion
 
-
-
-
-
-
-
-
-
-
-
+print("welcome. My name is burt. i will be helping you today... 😈")
+while True:
+    question=input(">>>")
+    response=chat(question)
+    print( response["choices"][0]["message"]["content"])
     
+
+
+
+
+
+
+
+
+
+
+        
