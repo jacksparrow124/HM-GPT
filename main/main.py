fa47142ea40f5861959a31cf1850769e27ee8091
jacksparrow124 <130068292+jacksparrow124@users.noreply.github.com>
2023-05-27 08:40:58 -0700

    Update main.py

--- main/main.py
+++ main/main.py
@@ -11,15 +11,6 @@ import countio
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY2")
 
-#def speechToText(mp3):
-    #input = audio_file("path/to/file/audio.mp3")
-    #put the folder you want to save the file to in the .mp3 above
-    #audio_file = open(input)
-    #transcript = openai.Audio.transcribe("whisper-1", audio_file)
-
-#commenting out open function block that was causing compile errors. Uncomment when there is code to put in here
-#def textToSpeech(text):
-       #text to speech using whisper library 
 
 def chat(input): 
     completion = openai.ChatCompletion.create(
@@ -36,14 +27,3 @@ while True:
    # print(response) (for debugging)
     print(response["choices"][0]["message"]["content"]) #type: ignore
 
-
-
-
-
-
-
-
-
-
-
-        
